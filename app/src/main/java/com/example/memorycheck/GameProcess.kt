8d39091ede1e9f1ade93378bba4add263af074a5
Violetta-Mamykina <violetta-1998m@mail.ru>
2020-01-16 04:40:49 +0300

    - add medium and hard levels - change icon - English and Russian modes - change end of game

--- app/src/main/java/com/example/memorycheck/GameProcess.kt
+++ app/src/main/java/com/example/memorycheck/GameProcess.kt
@@ -1,15 +1,14 @@
 package com.example.memorycheck
 
 import android.os.Bundle
-
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Context
 import android.view.WindowManager
 import android.widget.GridView
 import android.widget.AdapterView.OnItemClickListener
-import android.widget.Toast
-
 import java.util.ArrayList
+
 
 @Suppress("DEPRECATION")
 class GameProcess : Activity() {
@@ -17,13 +16,20 @@ class GameProcess : Activity() {
     private lateinit var grid: GridView
     private lateinit var adapter: GridAdapter
     private lateinit var pictures: ArrayList<String>
-
-    var GRID_COLUMN = 4
+    var GRID_COLUMN = 0
     var GRID_ROW = 3
+
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_process)
+        val level = intent.getIntExtra("level", -1)
+        when (level) {
+            0 -> GRID_COLUMN = 4
+            1 -> GRID_COLUMN = 6
+            2 -> GRID_COLUMN = 8
+        }
+
         grid = findViewById<GridView>(R.id.field)
         grid.numColumns = GRID_COLUMN
         grid.isEnabled = true
@@ -39,9 +45,20 @@ class GameProcess : Activity() {
             OnItemClickListener { parent, v, position, id ->
                 adapter.checkOpenCells()
                 adapter.openCell(position)
-                //изменить конец игры
-                if (adapter.checkGameOver())
-                    Toast.makeText(applicationContext, "Игра закончена", Toast.LENGTH_SHORT).show()
+
+                if (adapter.checkGameOver()) {
+                    val alertDialog = AlertDialog.Builder(this)
+                    alertDialog.setTitle(getString(R.string.end))
+                    alertDialog.setPositiveButton(getString(R.string.repeat_game)) { dialog, which ->
+                        finish()
+                        startActivity(intent)
+                    }
+
+                    alertDialog.setNegativeButton(getString(R.string.exit)) { dialog, which ->
+                        finish()
+                    }
+                    alertDialog.show()
+                }
             }
     }
 }
--- app/src/main/java/com/example/memorycheck/MainActivity.kt
+++ app/src/main/java/com/example/memorycheck/MainActivity.kt
@@ -10,10 +10,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val startGame = findViewById<Button>(R.id.startGameEasy)
-
-        startGame.setOnClickListener {
-            val intent = Intent(this@MainActivity, GameProcess::class.java)
+        val startGameEasy = findViewById<Button>(R.id.startGameEasy)
+        val startGameMedium = findViewById<Button>(R.id.startGameMedium)
+        val startGameHard = findViewById<Button>(R.id.startGameHard)
+        val intent = Intent(this@MainActivity, GameProcess::class.java)
+        startGameEasy.setOnClickListener {
+            intent.putExtra("level", 0)
+            startActivity(intent)
+        }
+        startGameMedium.setOnClickListener {
+            intent.putExtra("level", 1)
+            startActivity(intent)
+        }
+        startGameHard.setOnClickListener {
+            intent.putExtra("level", 2)
             startActivity(intent)
         }
     }
